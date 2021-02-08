7eea008c44ee32bc33cb6fa3c835b15963ab37ce
Nick <nmcarducci@gmail.com>
2021-02-08 05:37:16 -0500

    ok

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1
+        style={{
+          color: "rgb(200, 100, 100)",
+          fontFamily: '"Pacifico", sans-serif'
+        }}
+      >
+        Foie gras
+      </h1>
+    </div>
+  );
+}
