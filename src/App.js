248cd7f2e4793d42edc25f6195c4a91ef338ba2d
Nick Carducci <nmcarducci@gmail.com>
2021-03-05 21:23:08 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1
-        style={{
-          color: "rgb(200, 100, 100)",
-          fontFamily: '"Pacifico", sans-serif'
-        }}
-      >
-        Foie gras
-      </h1>
-      <iframe
-      width="300px"
-      src="https://www.youtube.com/embed/FySN9a4P168"
-      title="https://www.youtube.com/embed/FySN9a4P168"
-      ></iframe>
+      <div style={{ width: "100%", maxWidth: "600px", textAlign: "center" }}>
+        <h1
+          style={{
+            color: "rgb(200, 100, 100)",
+            fontFamily: '"Pacifico", sans-serif'
+          }}
+        >
+          Foie gras
+        </h1>
+        <iframe
+          width="300px"
+          src="https://www.youtube.com/embed/FySN9a4P168"
+          title="https://www.youtube.com/embed/FySN9a4P168"
+        ></iframe>
+      </div>
     </div>
   );
 }
