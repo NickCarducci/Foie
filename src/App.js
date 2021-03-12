677ad3f15c1f73894c3d22c6cdc3d3b2adc3493e
Nick Carducci <nmcarducci@gmail.com>
2021-03-12 14:45:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,13 +10,27 @@ export default function App() {
             fontFamily: '"Pacifico", sans-serif'
           }}
         >
-         <a href="https://micro-theory.com">Foie gras</a>
+          <a href="https://micro-theory.com">Foie gras</a>
         </h1>
         <iframe
           width="300px"
           src="https://www.youtube.com/embed/FySN9a4P168"
           title="https://www.youtube.com/embed/FySN9a4P168"
         ></iframe>
+        <div
+          style={{
+            display: "flex",
+            position: "fixed",
+            bottom: "0px",
+            right: "0px",
+            width: "min-content",
+            height: "min-content"
+          }}
+        >
+          Please help ouur consumers get what we lent them, destroy the&nbsp;
+          <a href="https://saverparty.xyz">Saver Party</a>'s&nbsp;equity
+          do not let the&nbsp;<a href="https://nickcarducci2022.com">Senate</a>&nbsp;destroy&nbsp;<a href="https://micro-theory.com">void</a>&nbsp;our promises
+        </div>
       </div>
     </div>
   );
